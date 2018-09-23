a479a73e9124c8c8c784c1c17a8091454206befc
Tim Barber <tfbninja@users.noreply.github.com>
2018-09-23 15:35:26 -0700

    Yeet

--- wack/lab03a_oop_return_labs/QuadraticRunner.java
+++ wack/lab03a_oop_return_labs/QuadraticRunner.java
@@ -5,7 +5,6 @@ package lab03a_oop_return_labs;
 //Date -
 //Class -
 //Lab  -
-
 import java.util.Scanner;
 import static java.lang.System.*;
 import static java.lang.Math.*;
@@ -13,6 +12,20 @@ import static java.lang.Math.*;
 public class QuadraticRunner {
 
     public static void main(String[] args) {
-        //add test cases
+        int a, b, c;
+        Quadratic test = new Quadratic();
+
+        for (int i = 0; i <= 2; i++) {
+
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print("Enter a :: ");
+            a = keyboard.nextInt();
+            System.out.print("Enter b :: ");
+            b = keyboard.nextInt();
+            System.out.print("Enter c :: ");
+            c = keyboard.nextInt();
+
+            test.dewIt(a, b, c);
+        }
     }
 }
