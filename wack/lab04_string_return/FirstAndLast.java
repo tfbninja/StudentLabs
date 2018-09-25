ccbc393c74b80c18bd09c9065f036162ff018f64
Tim Barber <tfbninja@users.noreply.github.com>
2018-09-24 17:40:23 -0700

    Yeetus

--- wack/lab04_string_return/FirstAndLast.java
+++ wack/lab04_string_return/FirstAndLast.java
@@ -5,29 +5,47 @@ package lab04_string_return;
 //Date -
 //Class -
 //Lab  -
-
 import static java.lang.System.*;
 
 public class FirstAndLast {
 
     private String word;
-    private char firstLetter;
-    private char lastLetter;
+    private String firstLetter;
+    private String lastLetter;
 
     public FirstAndLast() {
+        word = "";
+        firstLetter = "-1";
+        lastLetter = "-1";
     }
 
     public FirstAndLast(String s) {
+        word = s;
     }
 
     public void setString(String s) {
+        word = s;
     }
 
     public void findFirstLastLetters() {
+        firstLetter = word.substring(0,1);
+        lastLetter = word.substring(word.length() - 1);
     }
 
-    public String toString() {
-        String output = "";
-        return output;
+    public String toString(int whichOne) {
+        if (whichOne == 0) {
+            String output = firstLetter + " " + lastLetter;
+            return output;
+        }
+        if (whichOne == 1) {
+            return firstLetter;
+        }
+        if (whichOne == 2) {
+            return lastLetter;
+        }
+        else {
+            return firstLetter + " " + lastLetter;
+        }
+
     }
 }
