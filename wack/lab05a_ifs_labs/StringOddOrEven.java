5287ad5e93b6cf9dd38e2dbf0e203521e2237b39
Tim Barber <tfbninja@users.noreply.github.com>
2018-09-24 19:34:37 -0700

    Here everythings soft and smooth.

--- wack/lab05a_ifs_labs/StringOddOrEven.java
+++ wack/lab05a_ifs_labs/StringOddOrEven.java
@@ -5,29 +5,42 @@ package lab05a_ifs_labs;
 //Date - 
 //Class -
 //Lab  -
-
 import static java.lang.System.*;
 import java.util.Scanner;
 
 public class StringOddOrEven {
 
     private String word;
+    private int wordLen;
+    private String evenStr;
 
     public StringOddOrEven() {
+        word = "";
+        wordLen = -1;
     }
 
     public StringOddOrEven(String s) {
+        word = s;
+        wordLen = word.length();
     }
 
     public void setString(String s) {
+        word = s;
+        wordLen = word.length();
     }
 
     public boolean isEven() {
-        return false;
+        if (wordLen % 2 == 0) {
+            evenStr = "even";
+            return true;
+        } else {
+            evenStr = "odd";
+            return false;
+        }
     }
 
     public String toString() {
-        String output = "";
+        String output = word + " is " + evenStr;
         return output;
     }
 }
