39addb21d0dbfcff26290d0afc9af97bef908859
Tim Barber <tfbninja@users.noreply.github.com>
2018-10-07 19:45:06 -0700

    finished

--- wack/lab06a_if_else_labs/StringFirstLetterCheck.java
+++ wack/lab06a_if_else_labs/StringFirstLetterCheck.java
@@ -6,26 +6,40 @@ package lab06a_if_else_labs;
 //Class -
 //Lab  -
 
-import static java.lang.System.*;
-
 public class StringFirstLetterCheck {
 
+    char char1, char2;
     String wordOne, wordTwo;
 
     public StringFirstLetterCheck() {
+        char1 = ' ';
+        char2 = ' ';
+        wordOne = "";
+        wordTwo = "";
     }
 
     public StringFirstLetterCheck(String one, String two) {
+        char1 = one.charAt(0);
+        char2 = two.charAt(0);
+        wordOne = one;
+        wordTwo = two;
     }
 
     public void setWords(String one, String two) {
+        char1 = one.charAt(0);
+        char2 = two.charAt(0);
+        wordOne = one;
+        wordTwo = two;
     }
 
     public boolean checkFirstLetter() {
-        return false;
+        return char1 == char2;
     }
 
     public String toString() {
+        if (checkFirstLetter()){
+            return wordOne + " has the same first letter as " + wordTwo + "\n";
+        }
         return wordOne + " does not have the same first letter as " + wordTwo + "\n";
     }
 }
--- wack/lab06a_if_else_labs/StringFirstLetterRunner.java
+++ wack/lab06a_if_else_labs/StringFirstLetterRunner.java
@@ -6,11 +6,25 @@ package lab06a_if_else_labs;
 //Class -
 //Lab  -
 
-import static java.lang.System.*;
-
 public class StringFirstLetterRunner {
 
     public static void main(String args[]) {
-        //add test cases
+        StringFirstLetterCheck test = new StringFirstLetterCheck("hello", "howdy");
+        System.out.println(test);
+
+        test.setWords("one", "two");
+        System.out.println(test);
+
+        test.setWords("three", "two");
+        System.out.println(test);
+
+        test.setWords("TCEA", "UIL");
+        System.out.println(test);
+
+        test.setWords("State", "Champions");
+        System.out.println(test);
+
+        test.setWords("ABC", "DEF");
+        System.out.println(test);
     }
 }
