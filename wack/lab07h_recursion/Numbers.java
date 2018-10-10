a07c3caf50735a99002a2d7cc9f69d80fa05c32d
Tim Barber <tfbninja@users.noreply.github.com>
2018-10-10 15:04:51 -0600

    mhm

--- wack/lab07h_recursion/Numbers.java
+++ wack/lab07h_recursion/Numbers.java
@@ -3,7 +3,7 @@
  * from zero up to the number entered in.
  * Example - If 3 were entered in, you would output "The sum of all numbers through 3 == 6" (0 + 1 + 2 + 3)
  */
-package your_package;
+package lab07h_recursion;
 
 /**
  *
@@ -12,12 +12,17 @@ package your_package;
 public class Numbers {
 
     public static void main(String[] args) {
-        int num = 10;
+        int num = 6;
+
         Numbers numObj = new Numbers();
         System.out.println("The sum of all numbers through " + num + "  == " + numObj.printNum(num));
     }
-	//Write the recursive method to sum of all of the integers from zero up to the number entered
+    //Write the recursive method to sum of all of the integers from zero up to the number entered
+
     public int printNum(int num) {
-        
+        if (num <= 1) {
+            return num;
+        }
+        return printNum(num - 1) + num;
     }
 }
