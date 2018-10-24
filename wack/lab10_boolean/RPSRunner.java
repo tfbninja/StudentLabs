a24c7f57ca1dd99dc18ee6fd61bb0acaaf5a93c4
Tim Barber <tfbninja@users.noreply.github.com>
2018-10-23 17:49:07 -0700

    yote

--- wack/lab10_boolean/RPSRunner.java
+++ wack/lab10_boolean/RPSRunner.java
@@ -1,28 +1,33 @@
 package lab10_boolean;
 
-// A+ Computer Science  -  www.apluscompsci.com
-//Name -  
-//Date -
-//Class -
-//Lab  -
-
-import java.util.Scanner;
 import static java.lang.System.*;
+import java.util.Scanner;
 
 public class RPSRunner {
 
     public static void main(String args[]) {
         Scanner keyboard = new Scanner(System.in);
-        char response;
-
+        RockPaperScissors game = new RockPaperScissors();
+        String choice;
+        boolean again = false;
         //add in a do while loop after you get the basics up and running
 
-        String player = "";
+        do {
+            out.print("type in your prompt [R,P,S,L,K] :: ");
+            choice = keyboard.next();
+            game.setPlayers(choice);
+            game.setComputer();
+            System.out.println(game);
+            System.out.print("Do you want to play again?: ");
+            String temp = keyboard.next();
+            temp = temp.substring(0, 1).toLowerCase();
+            if (temp.equals("y")) {
+                again = true;
+            } else {
+                again = false;
+            }
 
-        out.print("type in your prompt [R,P,S] :: ");
+        } while (again);
 
-        //read in the player value
-
-        RockPaperScissors game = new RockPaperScissors();
     }
 }
--- wack/lab10_boolean/RockPaperScissors.java
+++ wack/lab10_boolean/RockPaperScissors.java
@@ -1,15 +1,15 @@
 package lab10_boolean;
 
-import java.util.Scanner;
-import static java.lang.System.*;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 public class RockPaperScissors {
 
     private String playChoice;
     private String compChoice;
     private Map<String, String> winner = new HashMap<>();
+    private int playWins = 0;
+    private int compWins = 0;
 
     public RockPaperScissors() {
         winner.put("paper rock", "paper");
@@ -27,33 +27,73 @@ public class RockPaperScissors {
     }
 
     public RockPaperScissors(String player) {
-        this.playChoice = player;
+        this.setPlayers(player);
     }
 
     public void setPlayers(String player) {
+        player = player.substring(0, 1).toLowerCase();
+        if (player.equals("r")) {
+            player = "rock";
+        } else if (player.equals("p")) {
+            player = "paper";
+        } else if (player.equals("s")) {
+            player = "scissors";
+        } else if (player.equals("l")) {
+            player = "lizard";
+        } else {
+            player = "spock";
+        }
+        winner.put("paper rock", "paper");
+        winner.put("rock scissors", "rock");
+        winner.put("paper scissors", "scissors");
+        winner.put("lizard scissors", "scissors");
+        winner.put("paper spock", "paper");
+        winner.put("scissors spock", "spock");
+        winner.put("lizard rock", "rock");
+        winner.put("lizard spock", "lizard");
+        winner.put("rock spock", "spock");
+        winner.put("lizard paper", "lizard");
         this.playChoice = player;
     }
 
     public String setComputer() {
         double choice = Math.random();
-        if (choice < 1 / 3.0) {
+        if (choice < 0.2) {
             return this.compChoice = "rock";
-        } else if (choice < 2 / 3.0) {
+        } else if (choice < 0.4) {
             return this.compChoice = "scissors";
-        } else {
+        } else if (choice < 0.6) {
             return this.compChoice = "paper";
+        } else if (choice < 0.8) {
+            return this.compChoice = "lizard";
+        } else {
+            return "spock";
         }
     }
 
     public String determineWinner() {
-        String winner = "";
-        if () {
-            return winner;
+        String builder;
+        if (this.playChoice.compareTo(this.compChoice) < 0) {
+            builder = this.playChoice + " " + this.compChoice;
+        } else {
+            builder = this.compChoice + " " + this.playChoice;
         }
+        return winner.get(builder);
     }
 
     public String toString() {
-        String output = "";
+        String output = "Player had " + this.playChoice + "\nComputer had " + this.compChoice + "\n";
+        System.out.println(this.playChoice);
+        if (this.playChoice.equals(this.compChoice)) {
+            output = "Player had " + this.playChoice + "\nComputer had " + this.compChoice + "\n!Draw Game!";
+        } else if (determineWinner().equals(this.playChoice)) {
+            output += "!Player wins <<" + this.playChoice.substring(0, 1).toUpperCase() + this.playChoice.substring(1) + " beats " + this.compChoice.substring(0, 1).toUpperCase() + this.compChoice.substring(1) + ">>!";
+            this.playWins++;
+        } else {
+            output += "!Computer wins <<" + this.compChoice.substring(0, 1).toUpperCase() + this.compChoice.substring(1) + " beats " + this.playChoice.substring(0, 1).toUpperCase() + this.playChoice.substring(1) + ">>!";
+            this.compWins++;
+        }
+        output += "\nYour wins: " + this.playWins + "\nComputer wins: " + this.compWins;
         return output;
     }
 }
