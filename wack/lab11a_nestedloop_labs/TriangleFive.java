3be6d7d2dab647f184a074b04c025f7a30da6c2c
Tim Barber <tfbninja@users.noreply.github.com>
2018-10-29 23:02:54 -0700

    i

--- wack/lab11a_nestedloop_labs/TriangleFive.java
+++ wack/lab11a_nestedloop_labs/TriangleFive.java
@@ -5,28 +5,40 @@ package lab11a_nestedloop_labs;
 //Date -
 //Class - 
 //Lab  -
-
-import static java.lang.System.*;
-
 public class TriangleFive {
 
     private char letter;
     private int amount;
 
     public TriangleFive() {
+        this.letter = '-';
+        this.amount = 0;
     }
 
     public TriangleFive(char c, int amt) {
+        this.letter = c;
+        this.amount = amt;
     }
 
     public void setLetter(char c) {
+        this.letter = c;
     }
 
     public void setAmount(int amt) {
+        this.amount = amt;
     }
 
     public String toString() {
         String output = "";
+        for (int i = 0; i < this.amount; i++) {
+            for (int count = this.amount; count > i; count--) {
+                for (int square = 0; square < count; square++) {
+                    output += (char) (((this.letter + (this.amount - count)) % 26) + 65);
+                }
+                output += " ";
+            }
+            output += "\n";
+        }
         return output;
     }
 }
--- wack/lab11a_nestedloop_labs/TriangleFiveRunner.java
+++ wack/lab11a_nestedloop_labs/TriangleFiveRunner.java
@@ -5,11 +5,16 @@ package lab11a_nestedloop_labs;
 //Date -
 //Class - 
 //Lab  -
-
-import static java.lang.System.*;
-
 public class TriangleFiveRunner {
 
     public static void main(String args[]) {
+        char[] testChars = {'C', 'A', 'B', 'X', 'Z'};
+        int[] testNums = {4, 5, 7, 6, 8};
+        TriangleFive tester = new TriangleFive();
+        for (int i = 0; i < testChars.length; i++) {
+            tester.setAmount(testNums[i]);
+            tester.setLetter(testChars[i]);
+            System.out.println(tester);
+        }
     }
 }
