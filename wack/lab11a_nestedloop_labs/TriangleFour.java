1669e2800452e0bcfc1c685ab5c1781b797deede
Tim Barber <tfbninja@users.noreply.github.com>
2018-10-29 17:27:53 -0700

    yote

--- wack/lab11a_nestedloop_labs/TriangleFour.java
+++ wack/lab11a_nestedloop_labs/TriangleFour.java
@@ -5,29 +5,41 @@ package lab11a_nestedloop_labs;
 //Date -
 //Class -
 //Lab  -
-
-import static java.lang.System.*;
-
 public class TriangleFour {
 
     private int size;
     private String letter;
 
     public TriangleFour() {
+        this.size = 0;
+        this.letter = "";
     }
 
     public TriangleFour(int count, String let) {
+        this.size = count;
+        this.letter = let;
     }
 
     public void setTriangle(String let, int sz) {
+        this.size = sz;
+        this.letter = let;
     }
 
     public String getLetter() {
-        return "#";
+        return this.letter;
     }
 
     public String toString() {
         String output = "";
+        for (int iteration = 0; iteration < this.size; iteration++) {
+            for (int spaces = 0; spaces < iteration; spaces++) {
+                output += " ";
+            }
+            for (int chars = this.size - iteration; chars > 0; chars--) {
+                output += this.letter;
+            }
+            output += "\n";
+        }
         return output + "\n";
     }
 }
--- wack/lab11a_nestedloop_labs/TriangleFourRunner.java
+++ wack/lab11a_nestedloop_labs/TriangleFourRunner.java
@@ -5,10 +5,17 @@ package lab11a_nestedloop_labs;
 //Date -
 //Class - 
 //Lab  -
-import static java.lang.System.*;
-
 public class TriangleFourRunner {
 
     public static void main(String args[]) {
+        TriangleFour tester = new TriangleFour();
+        String[] testStrings = {"R", "B", "X", "E", "T"};
+        int[] testSizes = {3, 4, 5, 2, 1};
+        int index = 0;
+        for (String testString : testStrings) {
+            tester.setTriangle(testString, testSizes[index]);
+            System.out.println(tester);
+            index++;
+        }
     }
 }
