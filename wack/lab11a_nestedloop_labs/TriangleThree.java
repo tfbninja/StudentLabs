e2734a1ae279d1787f39cd12e99d5b99e8b94298
Tim Barber <tfbninja@users.noreply.github.com>
2018-10-29 17:21:58 -0700

    yee

--- wack/lab11a_nestedloop_labs/TriangleThree.java
+++ wack/lab11a_nestedloop_labs/TriangleThree.java
@@ -5,29 +5,41 @@ package lab11a_nestedloop_labs;
 //Date -
 //Class -
 //Lab  -
-
-import static java.lang.System.*;
-
 public class TriangleThree {
 
     private int size;
     private String letter;
 
     public TriangleThree() {
+        this.size = 0;
+        this.letter = "";
     }
 
     public TriangleThree(int count, String let) {
+        this.size = count;
+        this.letter = let;
     }
 
     public void setTriangle(String let, int sz) {
+        this.size = sz;
+        this.letter = let;
     }
 
     public String getLetter() {
-        return "#";
+        return this.letter;
     }
 
     public String toString() {
         String output = "";
+        for (int a = 1; a <= this.size; a++) {
+            for (int b = this.size - a; b > 0; b--) {
+                output += " ";
+            }
+            for (int c = 1; c <= a; c++) {
+                output += this.letter;
+            }
+            output += "\n";
+        }
         return output + "\n";
     }
 }
--- wack/lab11a_nestedloop_labs/TriangleThreeRunner.java
+++ wack/lab11a_nestedloop_labs/TriangleThreeRunner.java
@@ -5,11 +5,19 @@ package lab11a_nestedloop_labs;
 //Date -
 //Class - 
 //Lab  -
-
-import static java.lang.System.*;
-
 public class TriangleThreeRunner {
 
     public static void main(String args[]) {
+        int[] tests = {3, 7, 1, 5, 4};
+        String[] testStrings = {"|", "|", "|", "|", "|"};
+        // I changed these because on my netbeans the font isn't monospaced,
+        // and the letters look spaced wrong, even though it's correct.
+        TriangleThree tester = new TriangleThree();
+        int index = 0;
+        for (String test : testStrings) {
+            tester.setTriangle(test, tests[index]);
+            System.out.println(tester);
+            index++;
+        }
     }
 }
