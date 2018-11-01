9e577702f8752aaa7aa34aa14ce97ae399142a15
Tim Barber <tfbninja@users.noreply.github.com>
2018-11-01 11:26:51 -0600

    yee

--- wack/lab12_scanner_chopper/AverageRunner.java
+++ wack/lab12_scanner_chopper/AverageRunner.java
@@ -5,12 +5,18 @@ package lab12_scanner_chopper;
 //Date -
 //Class - 
 //Lab  -
-
 import java.util.Scanner;
 import static java.lang.System.*;
 
 public class AverageRunner {
 
     public static void main(String args[]) {
+        String[] tests = {"9 10 5 20", "11 22 33 44 55 66 77", "48 52 29 100 50 29", "0", "100 90 95 98 100 97"};
+        Average tester = new Average();
+
+        for (String test : tests) {
+            tester.setLine(test);
+            System.out.println(tester);
+        }
     }
 }
