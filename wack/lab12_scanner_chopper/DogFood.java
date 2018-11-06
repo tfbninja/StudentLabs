d5198770a2c7a52dc0f67546f1ff32209885287f
Tim Barber <tfbninja@users.noreply.github.com>
2018-11-06 13:08:03 -0700

    yee

--- wack/lab12_scanner_chopper/DogFood.java
+++ wack/lab12_scanner_chopper/DogFood.java
@@ -9,19 +9,35 @@ package lab12_scanner_chopper;
 import java.util.Scanner;
 import static java.lang.System.*;
 import java.lang.Math;
+import java.util.HashMap;
 
 public class DogFood {
 
     private double amount;
+    private static final HashMap<Integer, Double> cupsPerPound = new HashMap<>();
+    
+    static {
+        cupsPerPound.put(4,3.5);
+        cupsPerPound.put(7,7.0);
+        cupsPerPound.put(9,10.5);
+        cupsPerPound.put(19,14.0);
+        cupsPerPound.put(39,24.0);
+        cupsPerPound.put(59,31.5);
+        cupsPerPound.put(79,42.0);
+        cupsPerPound.put(10000,52.5);
+    }
 
     public DogFood() {
+        this.amount = 0;
     }
 
     public DogFood(String line) {
         Scanner chopper = new Scanner(line);
         double cups = 0;
 
-        //calc code goes here
+        while (chopper.hasNextInt()){
+            
+        }
 
     }
 
