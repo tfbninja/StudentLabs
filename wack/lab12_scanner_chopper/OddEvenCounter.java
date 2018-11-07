b4d4f3fd396180b868051173ae79b71fc8b8dcec
Tim Barber <tfbninja@users.noreply.github.com>
2018-11-06 20:22:40 -0800

    yee

--- wack/lab12_scanner_chopper/OddEvenCounter.java
+++ wack/lab12_scanner_chopper/OddEvenCounter.java
@@ -1,38 +1,51 @@
 package lab12_scanner_chopper;
 
+import java.util.Scanner;
 // A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
 //Lab  -
 
-import java.util.Scanner;
-import static java.lang.System.*;
-
 public class OddEvenCounter {
 
     private String line;
 
     public OddEvenCounter() {
+        this.line = "";
     }
 
     public OddEvenCounter(String s) {
+        this.line = s;
     }
 
     public void setLine(String s) {
+        this.line = s;
     }
 
     public int getEvenCount() {
         int count = 0;
+        Scanner chopper = new Scanner(this.line);
+        while (chopper.hasNextInt()){
+            if (chopper.nextInt() % 2 == 0){
+                count++;
+            }
+        }
         return count;
     }
 
     public int getOddCount() {
         int count = 0;
+        Scanner chopper = new Scanner(this.line);
+        while (chopper.hasNextInt()){
+            if (chopper.nextInt() % 2 == 1){
+                count++;
+            }
+        }
         return count;
     }
 
     public String toString() {
-        return "";
+        return "even count = " + this.getEvenCount() + "\nodd count = " + this.getOddCount();
     }
 }
--- wack/lab12_scanner_chopper/OddEvenCounterRunner.java
+++ wack/lab12_scanner_chopper/OddEvenCounterRunner.java
@@ -1,16 +1,25 @@
 package lab12_scanner_chopper;
 
+import java.util.Scanner;
 // A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
 //Lab  -
 
-import java.util.Scanner;
-import static java.lang.System.*;
-
 public class OddEvenCounterRunner {
 
     public static void main(String args[]) {
+        String tests = "9 10 5 20\n11 22 33 44 55 66 77\n3 5 3 1 5 7 5 4 3 2 7 8 9 9 9 6\n2 4 6 8 2 4 6\n1 3 5 7 9 1 3 5";
+        Scanner chopper = new Scanner(tests);
+        chopper.useDelimiter("\n");
+        OddEvenCounter tester = new OddEvenCounter();
+
+        while (chopper.hasNextLine()){
+            String test = chopper.nextLine();
+            System.out.println(test);
+            tester.setLine(test);
+            System.out.println(tester + "\n");
+        }
     }
 }
