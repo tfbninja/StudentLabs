89376ad8496ed38816c1eacd211167fa4dd3534e
Tim Barber <tfbninja@users.noreply.github.com>
2018-11-06 21:34:17 -0800

    yee

--- wack/lab12_scanner_chopper/DogFood.java
+++ wack/lab12_scanner_chopper/DogFood.java
@@ -5,26 +5,28 @@ package lab12_scanner_chopper;
 //Date -
 //Class - 
 //Lab  -
-
-import java.util.Scanner;
-import static java.lang.System.*;
-import java.lang.Math;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class DogFood {
 
     private double amount;
     private static final HashMap<Integer, Double> cupsPerPound = new HashMap<>();
-    
+
     static {
-        cupsPerPound.put(4,3.5);
-        cupsPerPound.put(7,7.0);
-        cupsPerPound.put(9,10.5);
-        cupsPerPound.put(19,14.0);
-        cupsPerPound.put(39,24.0);
-        cupsPerPound.put(59,31.5);
-        cupsPerPound.put(79,42.0);
-        cupsPerPound.put(10000,52.5);
+        /*
+         * this HashMap<Integer, Double>, cupsPerPound, represents the number of
+         * cups of dog food needed for a puppy weighing less than or equal to
+         * the Key<Integer>, and more than the previous Key<Integer>
+         */
+        cupsPerPound.put(4, 3.5);
+        cupsPerPound.put(7, 7.0);
+        cupsPerPound.put(9, 10.5);
+        cupsPerPound.put(19, 14.0);
+        cupsPerPound.put(39, 24.0);
+        cupsPerPound.put(59, 31.5);
+        cupsPerPound.put(79, 42.0);
+        cupsPerPound.put(10000, 52.5);
     }
 
     public DogFood() {
@@ -32,20 +34,28 @@ public class DogFood {
     }
 
     public DogFood(String line) {
-        Scanner chopper = new Scanner(line);
-        double cups = 0;
-
-        while (chopper.hasNextInt()){
-            
-        }
-
+        setLine(line);
     }
 
     public double getAmount() {
-        return 0;
+        return this.amount;
+    }
+
+    public void setLine(String line) {
+        Scanner chopper = new Scanner(line);
+        double cups = 0; // start with 0 cups needed
+
+        while (chopper.hasNextInt()) { // while there is another measurement...
+            int weight = chopper.nextInt(); // get the weight
+            while (!cupsPerPound.containsKey(weight)) { // assuming the weight isn't exactly on the line, add one until it is
+                weight++;
+            }
+            cups += cupsPerPound.get(weight); // then get the number of cups needed for that weight
+        }
+        this.amount = Math.ceil(cups / 60.0); // divide the cups by 60, and if there is any remainder, we need another one, hence Math.ceil()
     }
 
     public String toString() {
-        return "";
+        return (int) this.amount + " - 10 POUND BAGS";
     }
 }
--- wack/lab12_scanner_chopper/DogFoodRunner.java
+++ wack/lab12_scanner_chopper/DogFoodRunner.java
@@ -5,13 +5,16 @@ package lab12_scanner_chopper;
 //Date -
 //Class - 
 //Lab  -
-
-import java.util.Scanner;
-import static java.lang.System.*;
-import java.lang.Math;
-
 public class DogFoodRunner {
 
     public static void main(String[] args) {
+        String[] tests = {"6 10 14 18 25 32", "12 15 20 26 35 42", "33 38 45 51 60 65", "40 50 60 77 90 101"};
+        DogFood tester = new DogFood();
+
+        for (String test : tests) {
+            tester.setLine(test);
+            System.out.println(tester.getAmount());
+            System.out.println(tester + "\n");
+        }
     }
 }
