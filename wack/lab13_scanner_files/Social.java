66db8034add66fd8e2160ed5f78672157c123b8c
Tim Barber <tfbninja@users.noreply.github.com>
2018-11-07 21:42:46 -0800

    yee

--- wack/lab13_scanner_files/Social.java
+++ wack/lab13_scanner_files/Social.java
@@ -1,38 +1,38 @@
 package lab13_scanner_files;
 
-// A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class - 
-//Lab  -
-
-import static java.lang.System.*;
+import java.util.Scanner;
 
 public class Social {
 
     private String socialNum;
 
     public Social() {
+        this.socialNum = "";
     }
 
     public Social(String soc) {
+        setSocial(soc);
     }
 
     public void setSocial(String soc) {
+        this.socialNum = soc;
+    }
+
+    public String getLine() {
+        return this.socialNum;
     }
 
     public int getSum() {
-
-
-
-
-
-
-
-        return 0;
+        Scanner chop = new Scanner(this.socialNum);
+        chop.useDelimiter("-");
+        int total = 0;
+        while (chop.hasNextInt()) {
+            total += chop.nextInt();
+        }
+        return total;
     }
 
     public String toString() {
-        return "";
+        return "SS# " + this.socialNum + " has a sum of " + this.getSum();
     }
 }
--- wack/lab13_scanner_files/SocialRunner.java
+++ wack/lab13_scanner_files/SocialRunner.java
@@ -5,15 +5,23 @@ package lab13_scanner_files;
 //Date -
 //Class - 
 //Lab  - 
-
-import java.io.IOException;
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
-import static java.lang.System.*;
 
 public class SocialRunner {
 
     public static void main(String args[]) throws IOException {
         Scanner file = new Scanner(new File("social.dat"));
+        Social tester = new Social();
+        file.useDelimiter("-");
+        file.nextLine();
+        while (file.hasNextLine()) {
+            tester.setSocial(file.nextLine()); // eliminate printing "SS# has a sum of 0"
+            if (!tester.getLine().equals("")) {
+                System.out.println(tester);
+            }
+        }
+
     }
 }
