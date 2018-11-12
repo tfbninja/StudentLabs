bcd18fe5cbd6708e15ce61825b05196cf68e0ef9
Tim Barber <tfbninja@users.noreply.github.com>
2018-11-12 09:44:31 -0800

    yee

--- wack/lab14a_array_labs/ArrayFunHouse.java
+++ wack/lab14a_array_labs/ArrayFunHouse.java
@@ -5,24 +5,37 @@ package lab14a_array_labs;
 //Date -
 //Class -
 //Lab  - 
-
-import java.lang.System;
-import java.lang.Math;
-
 public class ArrayFunHouse {
-    //instance variables and constructors could be used, but are not really needed
 
     //getSum() will return the sum of the numbers from start to stop, including stop
     public static int getSum(int[] numArray, int start, int stop) {
-        return 0;
+        int total = 0;
+        for (int i = start; i <= stop; i++) {
+            total += numArray[i];
+        }
+        return total;
     }
 
     //getCount() will return number of times val is present
     public static int getCount(int[] numArray, int val) {
-        return 0;
+        int count = 0;
+        for (int i : numArray) {
+            if (i == val) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static int[] removeVal(int[] numArray, int val) {
-        return null;
+        int[] newList = new int[numArray.length - getCount(numArray, val)];
+        int index = 0;
+        for (int i = 0; i < numArray.length; i++) {
+            if (numArray[i] != val) {
+                newList[index] = numArray[i];
+                index++;
+            }
+        }
+        return newList;
     }
 }
--- wack/lab14a_array_labs/ArrayFunHouseRunner.java
+++ wack/lab14a_array_labs/ArrayFunHouseRunner.java
@@ -1,11 +1,5 @@
 package lab14a_array_labs;
 
-// A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class -
-//Lab  - 
-
 import java.util.Arrays;
 
 public class ArrayFunHouseRunner {
@@ -18,11 +12,20 @@ public class ArrayFunHouseRunner {
         System.out.println("sum of spots 2-9  =  " + ArrayFunHouse.getSum(one, 2, 9));
         System.out.println("# of 4s  =  " + ArrayFunHouse.getCount(one, 4));
         System.out.println("# of 9s  =  " + ArrayFunHouse.getCount(one, 9));
+        System.out.println("# of 7s  =  " + ArrayFunHouse.getCount(one, 7));
+        System.out.println("new array with all 7s removed  = " + Arrays.toString(ArrayFunHouse.removeVal(one, 7)));
+        System.out.println("# of 7s = " + ArrayFunHouse.getCount(ArrayFunHouse.removeVal(one, 7), 7));
 
-        int[] two = {4, 2, 3, 4, 6, 7, 8, 9, 0, 10, 0, 1, 7, 6, 5, 3, 2, 9, 9, 8, 7};
+        int[] two = {7, 4, 2, 7, 3, 4, 6, 7, 8, 9, 7, 0, 10, 7, 0, 1, 7, 6, 5, 7, 3, 2, 7, 9, 9, 8, 7};
 
-        //add test cases
-
+        System.out.println("\n\n" + Arrays.toString(two));
+        System.out.println("sum of spots 3-16  =  " + ArrayFunHouse.getSum(two, 3, 16));
+        System.out.println("sum of spots 2-9  =  " + ArrayFunHouse.getSum(two, 2, 9));
+        System.out.println("# of 0s  =  " + ArrayFunHouse.getCount(two, 4));
+        System.out.println("# of 3s  =  " + ArrayFunHouse.getCount(two, 9));
+        System.out.println("# of 7s  =  " + ArrayFunHouse.getCount(two, 7));
+        System.out.println("new array with all 7s removed  = " + Arrays.toString(ArrayFunHouse.removeVal(two, 7)));
+        System.out.println("# of 7s = " + ArrayFunHouse.getCount(ArrayFunHouse.removeVal(two, 7), 7));
 
     }
 }
