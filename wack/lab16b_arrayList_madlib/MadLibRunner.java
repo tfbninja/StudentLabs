9f5a553e9ddbeccf9d968cd7c00b7fecda027849
Tim Barber <tfbninja@users.noreply.github.com>
2018-11-28 15:16:35 -0700

    yee

--- wack/lab16b_arrayList_madlib/MadLibRunner.java
+++ wack/lab16b_arrayList_madlib/MadLibRunner.java
@@ -1,11 +1,5 @@
 package lab16b_arrayList_madlib;
 
-// A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class - 
-//Lab  -
-
 import static java.lang.System.*;
 
 public class MadLibRunner {
