bf9ba91f6bd78245b2ba7947686b32f14a2c1c1e
Tim Barber <tfbninja@users.noreply.github.com>
2018-11-28 13:07:39 -0700

    yee

--- wack/lab16a_arrayList_prime/ArrayListFunHouse.java
+++ wack/lab16a_arrayList_prime/ArrayListFunHouse.java
@@ -5,7 +5,6 @@ package lab16a_arrayList_prime;
 //Date -
 //Class -
 //Lab  -
-
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
@@ -15,9 +14,25 @@ import static java.lang.System.*;
 public class ArrayListFunHouse {
 
     public static ArrayList<Integer> getListOfFactors(int number) {
-        return null;
+        ArrayList<Integer> factors = new ArrayList<>();
+        for (int a = 0; a <= (int) Math.sqrt((double) number); a++) {
+            for (int b = 0; b < number; b++) {
+                if (a * b == number && !factors.contains(a) && !factors.contains(b)) {
+                    factors.add(a);
+                    factors.add(b);
+                }
+            }
+        }
+        Collections.sort(factors);
+        return factors;
     }
 
     public static void keepOnlyCompositeNumbers(List<Integer> nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (getListOfFactors(nums.get(i)).size() > 2) {
+                nums.remove(i);
+                i--;
+            }
+        }
     }
 }
