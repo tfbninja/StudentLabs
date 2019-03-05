616bab3d3fe3e9d474affca5c5d020a7d8a07145
Tim Barber <tfbninja@users.noreply.github.com>
2019-03-05 12:29:56 -0700

    yee

--- wack/lab23b_recursion/Recursion234Runner.java
+++ wack/lab23b_recursion/Recursion234Runner.java
@@ -6,7 +6,7 @@ public class Recursion234Runner {
         int[] lab2TestCases = {4532, 1114532, 2245327, 2468, 13579};
         int[] answers2 = {2, 5, 3, 0, 5};
         for (int index = 0; index < answers2.length; index++) {
-            System.out.println("Odd digit cound should be " + answers2[index] + " and is " + RecursionFunOne.countOddDigits(lab2TestCases[index]));
+            System.out.println("Odd digit count should be " + answers2[index] + " and is " + RecursionFunOne.countOddDigits(lab2TestCases[index]));
         }
         int[] answers3 = {0, 1, 3, 0, 2};
         String[] lab3TestCases = {"itatfun", "itatchickenfun", "chchickchickenenicken", "chickchickfun", "chickenbouncetheballchicken"};
--- wack/lab23b_recursion/RecursionFunTwo.java
+++ wack/lab23b_recursion/RecursionFunTwo.java
@@ -5,13 +5,6 @@ import static java.lang.System.*;
 public class RecursionFunTwo {
 
     public static int countChickens(String word) {
-        if (word.length() <= 7) {
-            return word.toLowerCase().equals("chicken") ? 1 : 0;
-        }
-        if (word.toLowerCase().contains("chicken")) {
-            return 1 + countChickens(word.substring(word.indexOf("chicken"), word.indexOf("chicken") + 7));
-        } else {
-            return 0;
-        }       
+        return word.toLowerCase().contains("chicken") ? 1 + countChickens(word.substring(0, word.indexOf("chicken")) + word.substring(word.indexOf("chicken") + 7)) : 0;
     }
 }
