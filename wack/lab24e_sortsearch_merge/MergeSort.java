7dfd22b9df6444293b95e57f22b4eafc63ae23e6
Tim Barber <tfbninja@users.noreply.github.com>
2019-03-11 22:01:05 -0700

    yee

--- wack/lab24e_sortsearch_merge/MergeSort.java
+++ wack/lab24e_sortsearch_merge/MergeSort.java
@@ -27,26 +27,8 @@ public class MergeSort {
     private static void merge(Comparable[] list, int front, int back) {
         int mid = (front + back) / 2;
         Comparable[] temp = new Comparable[back - front];
+        
 
-        int insert = front;
-        for (int i = front; i < mid; i++) {
-            if (list[i].compareTo(list[mid]) < 0) {
-                insert = i;
-                break;
-            }
-        }
-        int end = insert;
-        for (int i = mid; i < back; i++) {
-            if (list[i].compareTo(list[insert]) > 0) {
-                end = i;
-                break;
-            }
-        }
-
-        for (int i = mid; i < end; i++) {
-            swap(list, insert, i);
-            insert++;
-        }
         System.out.print("pass " + passCount++ + " ");
         MergeSortRunner.prettyPrint(list);
         System.out.println(front + ", " + mid + ", " + back);
